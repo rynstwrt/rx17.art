eaef986e9e30ad4ffbf60c3ec4c06f8156cfcbe2
Ryan Stewart <ryanstewartalex@gmail.com>
2022-06-06 20:59:46 -0500

    Fix Miscellaneous Issues

--- js/infoanimations.js
+++ js/infoanimations.js
@@ -3,9 +3,6 @@
 // fade in text
 // fade in template text "You can __ me and _______ below"
 // fade in text
-
-
-
 const infoTimeline = anime.timeline({ loop: false, autoplay: false, easing: "easeOutExpo", duration: 400 })
     .add({ // Hello!
         targets: "#info-page h2:first-of-type",
@@ -46,46 +43,3 @@ const infoTimeline = anime.timeline({ loop: false, autoplay: false, easing: "eas
         ],
         delay: anime.stagger(50, { start: 500 })
     })
-
-// const infoTimeline = anime.timeline({ loop: false, autoplay: false, easing: "easeOutExpo", duration: 400 })
-//     .add({ // Hello!
-//         targets: "#info-page h2:first-of-type",
-//         opacity: [0, 1],
-//         translateX: ["100vw", 0],
-//     })
-//     .add({ // I'm a DJ from Dallas, TX
-//         targets: "#info-page h2:nth-child(2)",
-//         opacity: [0, 1],
-//         translateX: ["-100vw", 0],
-//         delay: 500
-//     })
-//     .add({ // I'm a DJ from Dallas, TX highlight
-//         targets: "#info-page h2:nth-child(2) .highlight",
-//         opacity: [0, 1],
-//         //color: ["#111111", "#FFFFFF"],
-//         duration: 750,
-//     })
-//     .add({ // You can contact me and find my mixes below
-//         targets: "#info-page h2:nth-child(3)",
-//         opacity: [0, 1],
-//         translateX: ["100vw", 0],
-//     })
-//     .add({ // You can contact me and find my mixes below highlight #1
-//         targets: "#info-page h2:nth-child(3) .highlight:nth-child(1)",
-//         opacity: [0, 1],
-//         // color: ["#111111", "#FFFFFF"],
-//         duration: 750,
-//     })
-//     .add({ // You can contact me and find my mixes below highlight #2
-//         targets: "#info-page h2:nth-child(3) .highlight:nth-child(2)",
-//         opacity: [0, 1],
-//         // color: ["#111111", "#FFFFFF"],
-//         duration: 750,
-//     })
-    // .add({ // Social icons
-    //     targets: "#social-list-container a",
-    //     opacity: [0, 1],
-    //     // color: ["#111111", "#EF8354"],
-    //     duration: 650,
-    //     delay: anime.stagger(125, { start: 250 })
-    // })
